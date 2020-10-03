e1daab3832032d0100020cac7c6d3f1d824fe160
Agustin <ag.fierro@hotmail.com>
2020-10-03 11:40:11 -0300

    kaiampal

---
+++ src/Razas/Kaiampal.java
@@ -0,0 +1,37 @@
+package Razas;
+
+public class Kaiampal extends Raza{
+
+	private static int rangoMax = 10;
+	
+	public Kaiampal( Punto pos ) {
+		
+		super( 144 , pos , 29 );		
+				
+	}
+
+	@Override
+	public void descansar() {
+		super.descansar();
+				
+	}
+
+	@Override
+	public void atacar(Raza otro) {
+		super.atacar(otro);
+		if( super.posicion.distAPunto( otro.posicion ) < rangoMax ) {
+			otro.recibirAtaque( super.danoAtaque * 0.6 );
+		}
+	}
+
+	@Override
+	public void recibirAtaque(double dano) {
+		salud -= dano;
+		salud += super.salud * 0.10;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/Razas/KaiampalTest.java
@@ -0,0 +1,14 @@
+package Razas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class KaiampalTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
