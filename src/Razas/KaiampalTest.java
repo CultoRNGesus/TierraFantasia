7faaf0f4f807b5a19e24ed86f593b8b7400c68c8
Agustin <ag.fierro@hotmail.com>
2020-10-03 11:42:15 -0300

    kaiampal 2

--- src/Razas/KaiampalTest.java
+++ src/Razas/KaiampalTest.java
@@ -4,11 +4,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+
 class KaiampalTest {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
